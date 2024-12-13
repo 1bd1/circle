53f7e9fb3757da0047ba47b07dc9f3fdbcd253a0
Dibran <dibran.ibisi@gmail.com>
2024-12-13 16:35:54 +0100

    2nd version of the Circle homework

--- src/kruh.java
+++ src/kruh.java
@@ -1,1 +1,43 @@
+public class kruh {
+
+
+    public static void main(String[] args)
+
+    {
+
+// pouzite jednotky - cm
+
+double polomer = 100;
+
+     double obvodKruhu = vypocetObvoduKruhu(polomer);
+     double obsahKruhu = vypocetObsahuKruhu(polomer);
+
+
+        System.out.print("obvod kruhu s polomerom (cm): ");
+        System.out.println(polomer);
+        System.out.print("je (cm, zaokruhlene): ");
+        System.out.println(Math.round(obvodKruhu));
+
+
+        System.out.print("obsah kruhu s polomerom (cm): ");
+        System.out.println(polomer);
+        System.out.print("je (cm2, zaokruhlene): ");
+        System.out.println(Math.round(obsahKruhu));
+    }
+
+    public static double vypocetObvoduKruhu(double polomer)
+    {
+        return ((polomer*2)*Math.PI);
+    }
+
+
+    public static double vypocetObsahuKruhu(double polomer) {
+
+        return (Math.pow(polomer,2))*Math.PI;
+    }
+
+
+
+
+}
 
